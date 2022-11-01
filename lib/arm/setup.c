da7765cb67690b90e0e54ed42144c0b1e4cc417c
Jean-Philippe Brucker <jean-philippe@linaro.org>
2022-11-01 10:55:07 +0000

    arm: Move io_init after vm initialization
    To create shared pages, the NS_SHARED bit must be written into the
    idmap. Before VM initializations, idmap hasn't necessarily been created.
    To write shared pages, access must be done on a IPA with the NS_SHARED
    bit. When the stage-1 MMU is enabled, that bit is set in the PTE. But
    when the stage-1 MMU is disabled, then the realm must write to the IPA
    with NS_SHARED directly.
    
    To avoid changing the whole virtio infrastructure to support pre-MMU in
    a realm, move the IO initialization after MMU enablement.
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>
    Signed-off-by: Suzuki K Poulose <suzuki.poulose@arm.com>

--- lib/arm/setup.c
+++ lib/arm/setup.c
@@ -263,9 +263,6 @@ void setup(const void *fdt, phys_addr_t freemem_start)
 	/* cpu_init must be called before thread_info_init */
 	thread_info_init(current_thread_info(), 0);
 
-	/* mem_init must be called before io_init */
-	io_init();
-
 	timer_save_state();
 
 	ret = dt_get_bootargs(&bootargs);
@@ -276,6 +273,9 @@ void setup(const void *fdt, phys_addr_t freemem_start)
 
 	if (!(auxinfo.flags & AUXINFO_MMU_OFF))
 		setup_vm();
+
+	/* mem_init and setup_vm must be called before io_init */
+	io_init();
 }
 
 #ifdef CONFIG_EFI

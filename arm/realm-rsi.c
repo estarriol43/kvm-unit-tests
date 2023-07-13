548d58939728b9c62bf9d5a3ebd61ccc37175ac8
Joey Gouly <joey.gouly@arm.com>
2023-07-13 11:54:48 +0100

    arm: realm: Add RSI version test
    Add basic test for checking the RSI version command.
    
    Signed-off-by: Joey Gouly <joey.gouly@arm.com>
    Signed-off-by: Suzuki K Poulose <suzuki.poulose@arm.com>

---
+++ arm/realm-rsi.c
@@ -0,0 +1,51 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2022 Arm Limited.
+ * All rights reserved.
+ */
+
+#include <libcflat.h>
+#include <asm/io.h>
+#include <asm/page.h>
+#include <asm/processor.h>
+#include <asm/psci.h>
+#include <alloc_page.h>
+#include <asm/rsi.h>
+#include <asm/pgtable.h>
+#include <asm/processor.h>
+
+static void rsi_test_version(void)
+{
+	struct smccc_result res;
+	int ret, version;
+
+	report_prefix_push("version");
+
+	ret = __rsi_get_version(RSI_ABI_VERSION, &res);
+	if (ret < 0) {
+		report(false, "SMC_RSI_ABI_VERSION failed (%d)", ret);
+		return;
+	}
+
+	version = res.r1;
+	report(res.r0 == RSI_SUCCESS, "RSI ABI version %u.%u (expected: %u.%u)",
+	       RSI_ABI_VERSION_GET_MAJOR(version),
+	       RSI_ABI_VERSION_GET_MINOR(version),
+	       RSI_ABI_VERSION_GET_MAJOR(RSI_ABI_VERSION),
+	       RSI_ABI_VERSION_GET_MINOR(RSI_ABI_VERSION));
+	report_prefix_pop();
+}
+
+int main(int argc, char **argv)
+{
+	report_prefix_push("rsi");
+
+	if (!is_realm()) {
+		report_skip("Not a realm, skipping tests");
+		goto exit;
+	}
+
+	rsi_test_version();
+exit:
+	return report_summary();
+}
